6b4a58109c4c5c13c51c7f1db49eaf12286f1f4f
Trevor Miranda <trevor.miranda@protonmail.com>
2020-03-18 21:00:17 -0400

    Working model

--- malaria/train.py
+++ malaria/train.py
@@ -1,21 +1,58 @@
+import os
+
 import numpy as np
-from efficientnet.keras import EfficientNetB0, center_crop_and_resize, preprocess_input
+from efficientnet.keras import (EfficientNetB3, center_crop_and_resize,
+                                preprocess_input)
+from keras import callbacks
+from keras.models import load_model
 
 from . import prepare_data as pd
 
+BATCH_SIZE = 8
+EPOCHS = 100
+
 
 def train(cell_images_path):
-    model = EfficientNetB0(weights=None)
+    print('INSTANTIATING MODEL')
+    if os.path.exists('malaria.hdf5'):
+        print('LOADING MODEL')
+        model = load_model('malaria.hdf5')
+    else:
+        print('CREATING MODEL')
+        model = EfficientNetB3(classes=2, weights=None)
 
-    x_train, x_test, y_train, y_test = pd.prepare_data(
-        cell_images_path, model.input_shape[1])
+    print('LOADING IMAGE PATHS AND LABELS')
+    x_train, x_test, y_train, y_test = pd.prepare_data_lists(cell_images_path)
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='SGD', metrics=['accuracy'])
+                  optimizer='SGD',
+                  metrics=['accuracy'])
 
-    trained = model.fit(x_train, y_train, epochs=10, batch_size=32,
-                        verbose=1, workers=8, use_multiprocessing=True)
-    predict = model.evaluate(x_test, y_test)
+    print('TRAINING MODEL')
+    model.fit_generator(pd.provide_training_data(x_train, y_train, BATCH_SIZE,
+                                                 model.input_shape[1]),
+                        epochs=EPOCHS,
+                        steps_per_epoch=len(x_train) // BATCH_SIZE,
+                        verbose=1,
+                        callbacks=[
+                            callbacks.EarlyStopping(monitor='loss',
+                                                    min_delta=0.01,
+                                                    patience=3,
+                                                    mode='min',
+                                                    restore_best_weights=True),
+                            callbacks.ModelCheckpoint('malaria.hdf5',
+                                                      monitor='loss',
+                                                      mode='min',
+                                                      save_best_only=True),
+                        ])
+    print('EVALUATING MODEL')
+    predict = model.evaluate_generator(
+        pd.provide_validation_data(x_test, y_test, BATCH_SIZE,
+                                   model.input_shape[1]),
+        steps=len(x_test) // BATCH_SIZE,
+        verbose=1,
+    )
 
-    print(f'loss: {predict[0]}')
-    print(f'accuracy: {predict[1]}')
+    print(f'LOSS: {predict[0]}')
+    print(f'ACCURACY: {predict[1]}')
+    print(predict)
